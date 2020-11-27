a052b0a51695f26cf0d784417bd48b0ddd15c410
Github Deploy <deploy@github.com>
2020-11-27 22:25:50 +0000

    Deploy

---
+++ precache-manifest.85fa6a9f8f468b381329c76b66ba7a1b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7fe9fe0ff0fadeb2fb",
+    "url": "/js/about.0a340c11.js"
+  },
+  {
+    "revision": "302ab86dea4a65eac930",
+    "url": "/css/app.4e23dd61.css"
+  },
+  {
+    "revision": "302ab86dea4a65eac930",
+    "url": "/js/app.8e5710c2.js"
+  },
+  {
+    "revision": "2c81043240e0f21acc09",
+    "url": "/css/chunk-vendors.3958183b.css"
+  },
+  {
+    "revision": "2c81043240e0f21acc09",
+    "url": "/js/chunk-vendors.daac31a4.js"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "436aa79d72ff38a81ac53a3a9983e38e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "631460359c23165ece5842cdb006177a",
+    "url": "/192.png"
+  },
+  {
+    "revision": "1af82d4beb1d757ae6a422e7395734e0",
+    "url": "/512.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
